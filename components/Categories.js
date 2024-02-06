24e9003b55d94d4c4b52deb7b5f0e447e90b7366
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2024-02-06 20:59:18 +0100

    Starting Delivery App

---
+++ components/Categories.js
@@ -0,0 +1,26 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
+      {/* CategoryCard */}
+      <CategoryCard imgUrl="https://media-cdn.tripadvisor.com/media/photo-s/19/3b/00/06/sushi-place.jpg" title="Testing" />
+      <CategoryCard imgUrl="https://media-cdn.tripadvisor.com/media/photo-s/19/3b/00/06/sushi-place.jpg" title="Testing" />
+      <CategoryCard imgUrl="https://media-cdn.tripadvisor.com/media/photo-s/19/3b/00/06/sushi-place.jpg" title="Testing" />
+      <CategoryCard imgUrl="https://media-cdn.tripadvisor.com/media/photo-s/19/3b/00/06/sushi-place.jpg" title="Testing" />
+      <CategoryCard imgUrl="https://media-cdn.tripadvisor.com/media/photo-s/19/3b/00/06/sushi-place.jpg" title="Testing" />
+      <CategoryCard imgUrl="https://media-cdn.tripadvisor.com/media/photo-s/19/3b/00/06/sushi-place.jpg" title="Testing" />
+    </ScrollView>
+  );
+};
+
+export default Categories;
