a555eef10e10a9837784b04b0876d8cd0ea747a9
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2024-02-20 21:06:47 +0100

    Continue working on delivery app

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,6 +1,17 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { urlFor } from "../sanity";
+import {
+  MinusCircleIcon,
+  PlusCircleIcon,
+} from "react-native-heroicons/outline";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItems,
+  selectBasketItemsWithId,
+} from "../features/basketSlice";
 
 const DishRow = ({ id, name, description, price, image }) => {
   const formattedPrice = new Intl.NumberFormat("en-GB", {
@@ -8,14 +19,38 @@ const DishRow = ({ id, name, description, price, image }) => {
     currency: "GBP",
   }).format(price);
 
+  const [isPressed, setIsPressed] = useState(false);
+
+  const items = useSelector((state) => selectBasketItemsWithId(state, id));
+
+  const dispatch = useDispatch();
+
+  const addItemToBaskey = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }));
+  };
+
   return (
-    <TouchableOpacity>
-      <View>
-        <Text className="text-lg mb-1">{name}</Text>
-        <Text className="text-gray-400">{description}</Text>
-        <Text className="text-gray-400 mt-2">{formattedPrice}</Text>
-      </View>
-      <View>
+    <>
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        className={`bg-white border p-4 border-gray-200 ${
+          isPressed && "border-b-0"
+        } `}
+      >
+        <View className="flex-row">
+          <View className="flex-1 pr-2">
+            <Text className="text-lg mb-1">{name}</Text>
+            <Text className="text-gray-400">{description}</Text>
+            <Text className="text-gray-400 mt-2">{formattedPrice}</Text>
+          </View>
+
+          {/*<View>
         <Image
           style={{
             borderWidth: 1,
@@ -26,8 +61,29 @@ const DishRow = ({ id, name, description, price, image }) => {
           }}
           className="h-20 w-20 bg-gray-300 p-4"
         />
-      </View> 
-    </TouchableOpacity>
+        </View> */}
+        </View>
+      </TouchableOpacity>
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3">
+            <TouchableOpacity
+              disabled={!items.length}
+              onPress={removeItemFromBasket}
+            >
+              <MinusCircleIcon
+                color={items.length > 0 ? "#00CCBB" : "gray"}
+                size={40}
+              />
+            </TouchableOpacity>
+            <Text>{items.length}</Text>
+            <TouchableOpacity onPress={addItemToBaskey}>
+              <PlusCircleIcon color="#00CCBB" size={40} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </>
   );
 };
 
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -88,14 +88,16 @@ const RestaurantScreen = () => {
         <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
         {/* Dishrows*/}
         {dishes.map((dish) => {
-          <DishRow
-            key={dish._id}
-            id={dish._id}
-            name={dish.name}
-            description={dish.short_description}
-            price={dish.price}
-            image={dish.image}
-          />;
+          return (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          );
         })}
       </View>
     </ScrollView>
